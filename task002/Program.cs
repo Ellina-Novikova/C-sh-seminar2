3a1f142e26f3062a327ec8f6e9343447497c2a33
Рабочая <yuo@exemple.com>
2022-09-20 01:18:06 +0700

    Трехзначное в двухзначное

---
+++ task002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вывести трехзначное число и удалить вторую цифру
+
+int CreateNumber()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+
+int DelSecondDigit(int number)
+{
+    int Sot = number / 100;
+    int Ed = number % 10;
+
+    int numberNew = Sot *10 + Ed;
+    return numberNew;
+}
+
+int a = CreateNumber();
+Console.WriteLine(a);
+int numberNew = DelSecondDigit(a);
+Console.WriteLine(numberNew);
