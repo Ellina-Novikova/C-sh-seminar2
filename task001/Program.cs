30dbc690ae2e229aa1d4a9c9cbeb0d5b0830ad9a
Рабочая <yuo@exemple.com>
2022-09-20 00:51:52 +0700

    методы

---
+++ task001/Program.cs
@@ -0,0 +1,33 @@
+﻿// void Insert(int a)
+// {
+ //   
+// }
+
+
+
+int CreateNumber()
+{
+    int number = new Random().Next(10, 100);
+    return number;
+}
+
+int MaxDigit(int number)
+{
+    int Decimal = number / 10;
+    int Ed = number % 10;
+
+    if (Decimal >= Ed)
+    {
+        return Decimal;
+    }
+    else
+    {
+        return Ed;
+    }
+
+}
+
+int a = CreateNumber();
+Console.WriteLine(a);
+int maxDigit = MaxDigit(a);
+Console.WriteLine(maxDigit);
