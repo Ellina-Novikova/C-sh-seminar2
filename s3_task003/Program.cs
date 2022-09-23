c56cfdcaed0cf2c8ac5bbb51cbab4421bbfcb5f2
Рабочая <yuo@exemple.com>
2022-09-23 22:46:18 +0700

    Расстояние м/у коорд. в 2d

---
+++ s3_task003/Program.cs
@@ -0,0 +1,19 @@
+﻿// Вводим координаты двух точек и 
+// вычисляем расстояние между ними в 2d пространстве
+
+double GetNumber(string name)
+{
+    Console.WriteLine("Введите координату " + name);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double GetDistance(double x1, double y1, double x2, double y2)
+{
+return Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)); 
+}
+
+double x1 = GetNumber("x1");
+double y1 = GetNumber("y1");
+double x2 = GetNumber("x2");
+double y2 = GetNumber("y2");
+Console.WriteLine("Расстояние между точками равно " + GetDistance(x1, y1, x2,y2));
