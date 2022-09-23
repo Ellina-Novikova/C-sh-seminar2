0f7086b522ca19fe72a8c1cfea11154264b6462f
Рабочая <yuo@exemple.com>
2022-09-23 22:27:04 +0700

    Кратность 7 и 23

---
+++ s3_task001/Program.cs
@@ -0,0 +1,49 @@
+﻿// Семинар 3. Задача 1. 
+// Принять число и проверить является ли оно кратным 7 и 23.
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число!");
+    return Convert.ToInt32(Console.ReadLine());     
+}
+
+bool CheckMod(int a, int mod)
+{
+    return (a % mod == 0);
+}
+
+int a = GetNumber();
+
+if (CheckMod(a,7) && CheckMod(a,23))
+    Console.WriteLine("Верно!");
+else
+    Console.WriteLine("Не верно!");
+
+
+
+//     if (a % 7 == 0)
+// {
+//     del7 = true;
+// }
+// else
+// {
+//     del7 = false;
+// }
+
+// if (a % 23 == 0)
+// {
+//     del23 = true;
+// }
+// else
+// {
+//     del23 = false;
+// }
+
+// if (del7==true && del23==true)
+// {
+//     result = true;
+// }
+// else
+// {
+//     result = false;
+// }
