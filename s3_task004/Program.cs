8b016dc05adc4507cc0c3120cbb2a3987e57e7d6
Рабочая <yuo@exemple.com>
2022-09-23 23:22:13 +0700

    Таблица квадратов от 1 до N

---
+++ s3_task004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Вводим число N и выводим таблицу квадратов от 1 до N
+int GetNumber()
+{
+    Console.WriteLine("Введите число ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintPow(int number)
+{
+int count = 1;
+while (count <= number)
+{
+    Console.WriteLine("Квадрат " + count + " = " + Math.Pow(count, 2));
+    count ++;
+}
+}
+
+PrintPow(GetNumber());
