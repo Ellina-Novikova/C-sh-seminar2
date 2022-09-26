fad86eb3da51a95d67ae09f393e9f835842477ac
Рабочая <yuo@exemple.com>
2022-09-27 02:39:40 +0700

    Количество цифр в числе

---
+++ s4_task002/Program.cs
@@ -0,0 +1,34 @@
+﻿// 
+// принимает число А и выдает количество цифр
+int GetNumber()
+{
+Console.WriteLine("Введите число");
+return Convert.ToInt32(Console.ReadLine());
+}
+    
+
+int GetDigCount(int A)  // количество цифр в числе
+{
+    int count = 0;
+    for(int i = 1; A > 0; i++)
+    {
+        A = A / 10;
+        count ++;
+    }
+    return count;
+}
+
+// int GetDigCount(int A)  // количество цифр в числе
+// {
+//     int count = 0;
+//     while (Math.Abs(A) > 0)
+//     {
+//         A = A / 10;
+//         count ++;
+//     }
+//     return count;
+// }
+
+
+int B = GetNumber();
+Console.WriteLine("В числе " + B + " количество цифр = " + GetDigCount(B));
