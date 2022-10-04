6fd08b0f603749aa124335b3d97cc76d646b9bb3
Рабочая <yuo@exemple.com>
2022-10-04 11:27:06 +0700

    Перевод числа в двоичную систему

---
+++ s6_task003/Program.cs
@@ -0,0 +1,41 @@
+﻿// Перевод числа из 10 системы в двоичную
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] Binar(int number)
+{
+    int index = 0;
+    int tempN = number;
+    while (tempN > 0)
+    {
+        index++;
+        tempN /= 2;
+    }
+    int[] bit = new int[index];
+    for (int i = 0; i < index; i++)
+    {        
+        bit[index - 1-i] = number % 2;
+        number /= 2;
+    }
+    return bit;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+int a = GetNumber();
+int[] binArray = Binar(a);
+PrintArray(binArray);
+PrintArray(Binar(a));
