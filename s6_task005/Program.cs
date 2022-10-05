0a2f270366fcb75379b257921237c313f3d33331
Рабочая <yuo@exemple.com>
2022-10-05 12:57:59 +0700

    Задача 45. Копирование массива

---
+++ s6_task005/Program.cs
@@ -0,0 +1,35 @@
+﻿// Программа создает копию заданного массива с помощью поэлементного копирования
+
+void FillArray(int[] arr) //Наполняем массив 
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-10, 10);        
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] copyArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        copyArr[i] = arr[i];
+    }
+    return copyArr;
+}
+
+int[] arrayFrom = new int[10];
+FillArray(arrayFrom);
+PrintArray(arrayFrom);
+int[] arrayTo = CopyArray(arrayFrom);
+PrintArray(arrayTo);
+
