43b62825cce7a97531433022800e28f67bd3b19e
Рабочая <yuo@exemple.com>
2022-10-07 02:08:29 +0700

    Задача 49. Заменить элементы с четными индексами

---
+++ s7_task003/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача 49
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+
+int[,] ChangeArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i+=2)
+    {
+        for(int j = 0; j < arr.GetLength(1); j+=2)
+        {
+            arr[i,j] = arr[i,j] * arr[i,j];
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(ChangeArray(array));
