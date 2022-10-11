bf74866e2a72a30c332d73b8cd0a2e9cda4ad726
Рабочая <yuo@exemple.com>
2022-10-11 15:48:33 +0700

    Результат деления и остаток

---
+++ s8_task003/Program.cs
@@ -0,0 +1,84 @@
+﻿// Частотный словарь двумерного массива
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+
+string[] SlovarArray(int[,] arr)
+{
+    string slovar = new string
+    int count = 0;
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i,j] == )
+        }
+        Console.WriteLine();
+    }
+}
+
+int CountDisting(int[,] arr)
+{
+    int count = 0;
+    int unicum = arr[arr.GetLength(0)-1,arr.GetLength(1)-1];
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i,j] != unicum)
+            {
+                count++;
+
+            }
+
+        }
+        unicum = arr[arr.GetLength(0) - 1 -- ,arr.GetLength(1) - 1 --];
+    }
+    return
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+
+PrintArray(array);
