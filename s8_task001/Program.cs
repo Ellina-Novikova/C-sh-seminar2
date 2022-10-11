9d37b5de23c12ece9b7bb491e4428f7cffd7d4b3
Рабочая <yuo@exemple.com>
2022-10-11 15:52:49 +0700

    Перестановка первой и последней строк масссива

---
+++ s8_task001/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача №53. Задать двумерный массив. Поменять первую и последнюю строки в массиве.
+
+int GetNumberFromConsole(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetRandomValue(int minValue, int maxValue)
+{
+    return new Random().Next(minValue, maxValue + 1);
+}
+
+void FillArray(int[,] arr, int minValue, int maxValue)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = GetRandomValue(minValue,maxValue);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] InitializateArray(int m, int n)
+{
+    return new int[m,n];
+}
+
+void ChangeArray(int[,] arr)
+{
+    int tempValue;    
+        for(int j = 0; j < arr.GetLength(1); j++)
+        {
+            tempValue = arr[0,j];
+            arr[0,j] = arr[arr.GetLength(0)-1,j];
+            arr[arr.GetLength(0)-1,j] = tempValue;
+        }    
+}
+
+//КОД ОСНОВНОЙ ПРОГРАММЫ
+int m = GetNumberFromConsole("Введите размерность массива m");
+int n = GetNumberFromConsole("Введите размерность массива n");
+int[,] array = InitializateArray(m,n);
+int minValue = GetNumberFromConsole("Введите минимальное число, допустимое в массиве");
+int maxValue = GetNumberFromConsole("Введите максимальное число, допустимое в массиве");
+FillArray(array,minValue,maxValue);
+
+PrintArray(array);
+ChangeArray(array);
+Console.WriteLine();
+PrintArray(array);
